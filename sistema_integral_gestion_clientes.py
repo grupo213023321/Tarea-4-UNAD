b0e789f2b012588ff552ecd6089408d6263f2967
Ronald Molina <ronaldstivenmolinad2@gmail.com>
2026-05-01 20:46:06 -0500

    función de prueba

---
+++ sistema_integral_gestion_clientes.py
@@ -0,0 +1,11 @@
+#archivo para la realización del trabajo grupal.
+#Función de prueba
+
+def sumar(a, b):
+    return a+b
+numero_1=int(input("Ingresa el primer numero: "))
+numero_2=int(input("Ingresa el siguiente número: "))
+a=numero_1
+b=numero_2
+suma = sumar(a,b)
+print(suma)
