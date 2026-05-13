c5f67bf73774dcbdaf1d441155aec874b729b582
Daniel Serrano Rivera <danny2204.dsr@gmail.com>
2026-05-12 22:08:46 -0500

    Ajuste en el main.py encargado de iniciar la GUI

--- main.py
+++ main.py
@@ -5,13 +5,11 @@
 # - Última actualización: 12 - 5 - 2026
 # - Aporte: Daniel Serrano Rivera
 #
-# Descripción: Punto de entrada del sistema Software FJ
-#              en modo consola. Contiene la clase Programa
-#              que implementa el menú interactivo y conecta
-#              todos los módulos del sistema. También
-#              pre-carga datos de prueba al iniciar.
+# Descripción: Modificado a solamente abrir la GUI
 #
 ##########################################################
+
+
 from interfaz import main
 
 if __name__ == "__main__":
